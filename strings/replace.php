ed4a8f52b84ca4755018ddeefa31e1b0a7d67000
Guz Alexander <kalimatas@gmail.com>
2014-11-25 12:36:30 +0300

    strings

--- strings/replace.php
+++ strings/replace.php
@@ -1,2 +1,17 @@
 <?php
 
+$str = 'abbple pear';
+$search = array('b', 'pear');
+$replace = array('pear', '');
+
+$r = str_replace($search, $replace, $str);
+var_dump($r); // "aple "
+
+$str = 'a b';
+$search = array('b', 'pear');
+$replace = array('pear', 'chiki');
+
+$r = str_replace($search, $replace, $str);
+var_dump($r); // "a chiki"
+
+echo PHP_EOL;
--- strings/search.php
+++ strings/search.php
@@ -7,4 +7,14 @@ var_dump(strspn('42 feoi', '09123465')); // 2
 // not
 var_dump(strcspn('42 feoi', 'f')); // 3
 
+$email = 'user@domain.org';
+
+echo strstr($email, '@') . PHP_EOL; // @domain.org
+echo strstr($email, '@', true) . PHP_EOL; // user
+
+$email = 'user@domain.org@another';
+echo strrchr($email, '@') . PHP_EOL; // @another, last occurence
+
+echo "5) " . var_export(substr(false, 0, 1), true) . PHP_EOL; // false -> ""
+
 echo PHP_EOL;
