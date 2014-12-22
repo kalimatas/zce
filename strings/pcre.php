7b864eef6f4c33c36f65edbf35274a3116cf0539
Guz Alexander <kalimatas@gmail.com>
2014-12-22 23:06:11 +0300

    php rocks pcre

--- strings/pcre.php
+++ strings/pcre.php
@@ -16,4 +16,28 @@ echo PHP_EOL;
 preg_match_all("/.*(\d{2,3}?).*$/m", "hello 423 \n no way 10", $matches);
 var_dump($matches);
 
+$phpRocks = "PhP5-rocks";
+
+$one = "/[a-z1-5\-]*/";
+$two = "/[php]{3}[1-5]{2,3}\-.*$/";
+$three = "/^[hp1-5]*\-.*/";
+$four = "/[hp1-5]*\-.?/";
+$five = " /[hp][1-5]*\-.*/";
+
+preg_match($one, $phpRocks, $matches);
+var_dump($matches); // matches
+
+preg_match($two, $phpRocks, $matches);
+var_dump($matches); // empty array
+
+preg_match($three, $phpRocks, $matches);
+var_dump($matches); // empty array
+
+preg_match($four, $phpRocks, $matches);
+var_dump($matches); // matches 5-r
+
+preg_match($five, $phpRocks, $matches);
+var_dump($matches); // empty array
+
+
 echo PHP_EOL;
