01e4d63d46e91baaf01a62c0f1bf39aeaf3e835e
Guz Alexander <kalimatas@gmail.com>
2014-12-23 17:59:32 +0300

    cURL

---
+++ web/curl.php
@@ -0,0 +1,27 @@
+<?php
+
+$url = 'google.com';
+
+$ch = curl_init();
+var_dump($ch); // resource(4) of type (curl)
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// adds options, not replaces
+curl_setopt_array($ch, [
+	CURLOPT_URL => $url,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_HEADER => true,
+	CURLOPT_NOBODY => true, // HEAD method
+	CURLOPT_VERBOSE => true,
+]);
+
+$result = curl_exec($ch);
+print_r(curl_error($ch));
+print_r(curl_getinfo($ch));
+print_r($result);
+
+curl_close($ch);
+var_dump($ch); // resource(4) of type (Unknown)
+
+echo PHP_EOL;
