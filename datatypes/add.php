8506b8db723f385849a16c916322b0e9b1754378
Guz Alexander <kalimatas@gmail.com>
2015-01-22 16:16:52 +0300

    xml

--- datatypes/add.php
+++ datatypes/add.php
@@ -3,11 +3,19 @@
 $xml = '<root />';
 
 $doc = new DOMDocument();
+$doc->formatOutput = true;
 $doc->loadXML($xml);
 
 $n = $doc->createElement('test', 'value');
 $doc->appendChild($n); // add on the same level as 'root'
 
-print_r($doc->saveXML());
+echo $doc->saveXML() . PHP_EOL;
+
+$doc->documentElement->appendChild($n); // append to 'root'
+
+echo $doc->saveXML();
+
+$docNew = new DOMDocument();
+var_dump($docNew->loadXML('')); // false and warning
 
 echo PHP_EOL;
